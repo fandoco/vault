5a86133930254027ff2ce048fcd355d30c1fb438
chanyadeshani <chanya.deshani@gmail.com>
2018-12-15 23:14:40 +0900

    adding get data

---
+++ src/main/resources/public/js/index.js
@@ -0,0 +1,131 @@
+function getDatabyType(type) {
+    let url1 = "https://fandoco-vault.herokuapp.com/data?type=" + type;
+
+//Fetch the content of the url using the XMLHttpRequest object
+    let req1 = new XMLHttpRequest();
+    req1.open("GET", url1);
+
+//req.setRequestHeader('Access-Control-Allow-Origin', '*')
+    req1.send(null);
+
+
+//register an event handler function
+
+    req1.onreadystatechange = function () {
+        if (req1.readyState === 4 && req1.status === 200) {
+            var response = req1.responseText;
+            var listOfSecureDetails = JSON.parse(response);
+
+            var list = "";
+            console.log(response);
+            for (let i = 0; i < listOfSecureDetails.length; i++) {
+                console.log("details", listOfSecureDetails[i]);
+                console.log(listOfSecureDetails.length);
+                let key = listOfSecureDetails[i].key;
+                let value = listOfSecureDetails[i].value;
+
+            }
+        }
+    }
+}
+
+
+function getTypes() {
+    let url2 = "https://fandoco-vault.herokuapp.com/types";
+
+//Fetch the content of the url using the XMLHttpRequest object
+    let req2 = new XMLHttpRequest();
+    req2.open("GET", url2);
+
+//req.setRequestHeader('Access-Control-Allow-Origin', '*')
+    req2.send(null);
+
+
+//register an event handler function
+
+    req2.onreadystatechange = function () {
+        if (req2.readyState === 4 && req2.status === 200) {
+            var response = req2.responseText;
+            var types = JSON.parse(response);
+
+            var list = "";
+            console.log(response);
+            for (let i = 0; i < types.length; i++) {
+                console.log("types", types[i]);
+                CreateSelectDropDown(types[i])
+
+            }
+        }
+    }
+}
+function myFunction() {
+    var table = document.getElementById("dataTable");
+    var row = table.insertRow(0);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    cell1.innerHTML = "NEW CELL1";
+    cell2.innerHTML = "NEW CELL2";
+}
+var a = ["a1", "b1", "c1", "d1", "d1"];
+
+//
+function CreateSelectDropDown(type) {
+    var z;
+    var t;
+
+    console.log(type)
+    //for (let i = 0; i < a.length; i++) {
+
+    z = document.createElement("option");
+    z.setAttribute("value", type);
+    t = document.createTextNode(type);
+    z.appendChild(t);
+    document.getElementById("bankname").appendChild(z);
+    //}
+}
+
+function showDetails() {
+    let e = document.getElementById("bankname");
+    let value = e.options[e.selectedIndex].value;
+    let selectedOption = e.options[e.selectedIndex].text;
+    getDatabyType(selectedOption);
+
+    /*switch (selectedOption) {
+        case "Post Bank Prasad":
+            document.getElementById("id").innerHTML = "id1";
+            document.getElementById("password").innerHTML = "password1";
+            break;
+        case "Post Bank Chanya":
+            document.getElementById("id").innerHTML = "id2";
+            document.getElementById("password").innerHTML = "password2";
+            break;
+        case "Rakuten Bank Prasad":
+            document.getElementById("id").innerHTML = "id3";
+            document.getElementById("password").innerHTML = "password3";
+            break;
+        case "Mizuho":
+            document.getElementById("id").value = "id4";
+            document.getElementById("password").innerHTML = "password4";
+            break;
+        case "North Pacific Prasad":
+            document.getElementById("id").innerHTML = "No Internet Banking";
+            document.getElementById("password").innerHTML = "";
+            break;
+        case "North Pacific Chanya":
+            document.getElementById("id").innerHTML = "No Internet Banking...";
+            document.getElementById("password").innerHTML = "";
+            break;
+        default:
+            document.getElementById("id").innerHTML = "id..";
+            document.getElementById("password").innerHTML = "password..";
+
+    }
+    */
+}
+
+function clearInformation() {
+
+    document.getElementById("bankname").selectedIndex = "0";
+    document.getElementById("id").innerHTML = "";
+    document.getElementById("password").innerHTML = "";
+}
